f7c4310bfa4ca513f6e235724282a33b959e5b3d
sErciyes <151975351+sErciyes@users.noreply.github.com>
2024-06-05 21:41:49 +0300

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'serdar_erciyas_prog3_final',
+  webDir: 'www'
+};
+
+export default config;
